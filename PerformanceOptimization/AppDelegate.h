ebee9c64dda2521a545015d418e20f4dcbe33de0
丁玉松 <yusong.ding@huangjinqianbao.com>
2018-12-07 17:00:01 +0800

    Initial Commit

---
+++ PerformanceOptimization/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PerformanceOptimization
+//
+//  Created by 丁玉松 on 2018/12/7.
+//  Copyright © 2018 丁玉松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
